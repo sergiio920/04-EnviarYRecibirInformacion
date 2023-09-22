4c538fb9905ba900eb548998ac0d168145a84129
sergiomoron <sergio.moronvidal@cieep.com>
2023-09-22 11:23:50 +0200

    empezamos con la creación de dirección

---
+++ app/src/main/java/sergio/moron/a04_enviaryrecibirinformacion/CrearDireccionActivity.java
@@ -0,0 +1,14 @@
+package sergio.moron.a04_enviaryrecibirinformacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearDireccionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_direccion);
+    }
+}
